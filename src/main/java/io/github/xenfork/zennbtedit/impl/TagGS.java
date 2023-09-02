af162ee4a4ca9298fc4344ff20003e704de93b94
baka4n <qq474899581@gmail.com>
2023-09-02 15:23:50 +0800

    update

--- src/main/java/io/github/xenfork/zennbtedit/impl/TagGS.java
+++ src/main/java/io/github/xenfork/zennbtedit/impl/TagGS.java
@@ -2,26 +2,15 @@ package io.github.xenfork.zennbtedit.impl;
 
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.item.IItemStack;
-import crafttweaker.api.minecraft.CraftTweakerMC;
-import net.minecraft.item.ItemStack;
 import stanhebben.zenscript.annotations.ZenClass;
-import stanhebben.zenscript.annotations.ZenGetter;
 import stanhebben.zenscript.annotations.ZenMethod;
 
 @SuppressWarnings("unused")
 @ZenClass("mods.zennbtedit.TagGS")
 @ZenRegister
-public class TagGS {
+public interface TagGS {
     @ZenMethod("nbt")
-    public static INBT getNbt(IItemStack stack) {
-        ItemStack itemStack = CraftTweakerMC.getItemStack(stack);
-        return (INBT)itemStack.getTagCompound();
-    }
+    INBT getNbt();
     @ZenMethod("nbt")
-    public static IItemStack setNbt(IItemStack stack, INBT nbt) {
-        ItemStack itemStack = CraftTweakerMC.getItemStack(stack);
-        itemStack.setTagCompound(nbt);
-        stack = CraftTweakerMC.getIItemStack(itemStack);
-        return stack;
-    }
+    IItemStack setNbt(INBT nbt);
 }
--- src/main/java/io/github/xenfork/zennbtedit/mixin/MCItemStackMixin.java
+++ src/main/java/io/github/xenfork/zennbtedit/mixin/MCItemStackMixin.java
@@ -1,24 +1,44 @@
 package io.github.xenfork.zennbtedit.mixin;
 
+import crafttweaker.api.item.IItemStack;
 import crafttweaker.mc1120.item.MCItemStack;
 import io.github.xenfork.zennbtedit.impl.INBT;
+import io.github.xenfork.zennbtedit.impl.TagGS;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import stanhebben.zenscript.annotations.ZenMethod;
 
+@Debug(export = true)
 @Mixin(MCItemStack.class)
-public class MCItemStackMixin {
-    private INBT zueNbt = null;
-    @Inject(method = "<init>(Lnet/minecraft/item/ItemStack;)V", at = @At("RETURN"))
+public class MCItemStackMixin implements TagGS {
+
+
+    @Shadow(remap = false) @Final private ItemStack stack;
+    @Mutable
+    @Final
+    private INBT zueNbt;
+    @Inject(method = "<init>(Lnet/minecraft/item/ItemStack;)V", at = @At("RETURN"), remap = false)
     private void init(ItemStack itemStack, CallbackInfo ci) {
         if (itemStack != null) {
             zueNbt = (INBT) itemStack.getTagCompound();
+        } else {
+            zueNbt = null;
         }
 
+    }
+    @Override
+    public INBT getNbt() {
+        return (INBT) stack.getTagCompound();
+//        ItemStack itemStack = CraftTweakerMC.getItemStack(stack);
+//        return (INBT)itemStack.getTagCompound();
+    }
+    @Override
+    public IItemStack setNbt(INBT nbt) {
+        ItemStack result = stack.copy();
+        result.setTagCompound(nbt);
+        return new MCItemStack(result);
     }
 }
